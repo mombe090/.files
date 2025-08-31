a662e2793e7228998c41f1025ec76114a4f5a59c
Mamadou Yaya DIALLO <yayamombeya090@gmail.com>
2025-08-31 04:07:17 -0400

    feat: add some hyperland customization

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function scold(msg)
+  vim.notify(msg, vim.log.levels.WARN, { title = "Stop using arrows!" })
+end
+
+-- Replace arrow keys with a funny warning
+local arrows = {
+  Up = "Use 'k' instead!",
+  Down = "Use 'j' instead!",
+  Left = "Use 'h' instead!",
+  Right = "Use 'l' instead!",
+}
+
+for key, message in pairs(arrows) do
+  for _, mode in ipairs({ "n" }) do
+    vim.keymap.set(mode, "<" .. key .. ">", function()
+      scold(message)
+    end, { noremap = true, silent = true })
+  end
+end
